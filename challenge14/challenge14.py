d990f88267fa471076fb3e89e6c40110eef0c716
Julia Van Cleve <julia@jmv.me>
2014-08-06 14:50:44 -0700

    Solved Challenge 14

--- challenge14/challenge14.py
+++ challenge14/challenge14.py
@@ -27,27 +27,44 @@ while delta > 0:
     green += colors[c:c+delta] # 99
     c = c+delta+1
     delta = delta-1
+    # 0,0 ... 99,0
+    # 1,1 ... 98,1
+    # 2,2 ... 97,2
     
-    pink += colors[c : c+delta] # 98
+    pink += colors[c:c+delta] # 98
     c = c+delta+1
+    # 99,1 99,2 99,3 99,4 ... 99,99
+    # 98,2 98,3           ... 98,98
+    # 97,3                ... 97,97
     
     red += colors[c:c+delta] # 98
     c = c+delta+1
     delta = delta-1
+    # 98,99 97,99 96,99 ... 0,99
+    # 97,98 96,98 96,98 ... 1,98
+    # 96,97             ... 2,97
     
-    purple += colors[c: c+delta] # 97
+    purple += colors[c:c+delta] # 97
     c = c+delta
+    # 0,98 0,97 .... 0,1
+    # 1,97 1,96 .... 1,2
+    # 2,96      .... 2,3
 
+p = 0
 low = 0
 high = 99
 for y in range(50):
-    p = 0
+
     for x in range(low, high):
-        new1.putpixel((x, y), green[p])
-        new2.putpixel((x, y), pink[p])
-        new3.putpixel((x, y), red[p])
-        new4.putpixel((x, y), purple[p])
-    p = p+1
+        if p < len(green):
+            new1.putpixel((x, y), green[p])
+        if p < len(pink):
+            new2.putpixel((x, y), pink[p])
+        if p < len(red):
+            new3.putpixel((x, y), red[p])
+        if p < len(purple):
+            new4.putpixel((x, y), purple[p])
+        p = p+1
     low = low+1
     high = high-1
     if low == high or low > high:
@@ -61,10 +78,6 @@ new1.show()
 new2.show()
 new3.show()
 new4.show()
-
-
-# 0,0 1,0 2,0 ... 97,0 98,0 99,0  # 0 - 99
-#     1,1 2,1...  97,1 98,1       # 1 - 98
     
 
 
